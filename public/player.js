146f450f89a7b197376e25c923273bc6f167b577
jay1905 <jaimeaughney@gmail.com>
2018-11-16 14:10:40 +0100

    player movement complete

---
+++ public/player.js
@@ -0,0 +1,51 @@
+class Player{
+
+  constructor(x=100,y=100){
+    this.x=x
+    this.y=y
+    this.speed=5
+    this.dirX=0
+    this.dirY=0
+    this.b = Bodies.rectangle(x, y, 32, 32)
+    this.b.label="player"
+    World.add(ourWorld, this.b);
+  }
+  setDirX(x){
+    this.dirX=x
+  }
+  setDirY(y){
+    this.dirY=y
+  }
+  move(){
+    switch (this.dirX) {
+      case 1:
+        Body.setVelocity( this.b, {x: this.speed, y: this.b.velocity.y})
+        break;
+      case -1:
+        Body.setVelocity( this.b, {x: -this.speed, y: this.b.velocity.y})
+        break;
+      case 0:
+        Body.setVelocity( this.b, {x:0, y: this.b.velocity.y});
+        break;
+    }
+    switch (this.dirY) {
+      case 1:
+        Body.setVelocity( this.b, {x: this.b.velocity.x, y: this.speed});
+        break;
+      case -1:
+        Body.setVelocity( this.b, {x: this.b.velocity.x, y: -this.speed});
+        break;
+      case 0:
+        Body.setVelocity( this.b, {x:this.b.velocity.x, y: 0});
+        break;
+    }
+  }
+  update(){
+
+
+    Body.setAngularVelocity(this.b, 0)
+    this.move()
+
+  }
+
+}
