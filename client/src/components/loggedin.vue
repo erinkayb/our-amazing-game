744e19f1945070b19ddb600b768a952b4bdabaa4
Erin de Blaeij <erinkayb@gmail.com>
2018-11-20 17:25:53 +0100

    login reroute to loggedin

---
+++ client/src/components/loggedin.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+  <v-container grid-list-xl text-md-center>
+    <v-layout row wrap>
+      <v-flex xs6>
+        <div class="white elevation-24">
+          <v-toolbar flat dense class='blue-grey darken-3' dark>
+            <v-toolbar-title>
+              Welcome
+            </v-toolbar-title>
+          </v-toolbar>
+          <div class='pl-4 pr-4 pt-4 pb-4'>
+            <v-flex>
+
+            </v-flex>
+
+        </div>
+        </div>
+      </v-flex>
+      <v-flex xs6>
+        <div class="white elevation-24">
+          <v-toolbar flat dense class='blue-grey darken-3' dark>
+            <v-toolbar-title>
+              Ready to play?
+            </v-toolbar-title>
+          </v-toolbar>
+          <div class='pl-4 pr-4 pt-4 pb-4'>
+            <v-flex>
+              <div class="text-xs-center">
+                <v-btn round block large class="orange darken-3" dark>Enter Game</v-btn>
+              </div>
+            </v-flex>
+
+        </div>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
