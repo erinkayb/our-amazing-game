eadb538e4be0baa2459a48f9ce8c2d76b3cd0658
Erin de Blaeij <erinkayb@gmail.com>
2018-11-26 15:29:00 +0100

    added logo and background. added ui to homepage

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,50 +1,57 @@
 <template>
   <v-container grid-list-xl text-md-center>
+    <div class="logo">
+      <img src="https://res.cloudinary.com/erinkayb/image/upload/v1543240847/radOrange.png" alt="">
+    </div>
     <v-layout row wrap>
-      <v-flex sm12>
-        <div class="white elevation-24">
-          <v-toolbar flat dense class=' light-green accent-4' dark>
-            <v-toolbar-title>
-              Welcome to our world
-            </v-toolbar-title>
-          </v-toolbar>
-          <div class='pl-4 pr-4 pt-4 pb-4'>
-            <v-flex>
-              <div class="text-xs-center">
-                <v-btn round block large class="orange darken-3" dark>Register</v-btn>
-                <v-btn round block large class="orange darken-3" dark>Login</v-btn>
-              </div>
-            </v-flex>
-          </div>
-        </div>
-      </v-flex>
-<!-- Ads -->
-      <v-flex xs4 mt-5 justify-space-around>
-            <div class='pl-4 pr-4 pt-4 pb-4'>
-                <v-img
-                  src="https://res.cloudinary.com/erinkayb/image/upload/v1542988856/Screen_Shot_2018-11-23_at_17.00.43.png"
-                  aspect-ratio="1.75"
-                ></v-img>
-            </div>
-      </v-flex>
-      <v-flex xs4 mt-5>
-            <div class='pl-4 pr-4 pt-4 pb-4'>
-                <v-img
-                  src="https://res.cloudinary.com/erinkayb/image/upload/v1542988710/ad.png"
+<!-- #e515e1 pink
+#0071bc blue
+#333 gray-->
 
-                ></v-img>
-            </div>
-      </v-flex>
-      <v-flex xs4 mt-5>
-            <div class='pl-4 pr-4 pt-4 pb-4'>
-                <v-img
-                  src="https://res.cloudinary.com/erinkayb/image/upload/v1542988710/ad.png"
+      <div class="wrap">
+    		<div class="clip-block">
+    			<div class="clip-each clip-solid">
+    			<div class="social-share-block">
+            <p>It's pretty simple, <br/> last shape standing wins. <br/>
+            Create an account and <br/>keep track of your wins and kills stats.
+            <br/>
+            <br/>
 
-                ></v-img>
-            </div>
-      </v-flex>
+            Movement: w, d, s, a
+            <br/>
+            Aim: mouse
+            <br/>
+            Shoot: left mouse click
+            <br/> </p>
+            <h1>How to play</h1>
+    			</div>
+    			</div>
+    		</div> <!-- /clip-block -->
 
-<!-- - -->
+    		<div class="clip-block">
+    			<a href="/#/register" class="clip-each clip-gradient">
+    				<div class="clip-caption">Sign Up</div>
+    			</a>
+    			<a href="/#/login" class="clip-each clip-border">
+    				<div class="clip-caption">Login</div>
+    			</a>
+    		</div> <!-- /clip-block -->
+    		<svg class="clip-svg">
+    			<defs>
+    				<clipPath id="hexagon-clip" clipPathUnits="objectBoundingBox">
+    					<polygon points="0.25 0.05, 0.75 0.05, 1 0.5, 0.75 0.95, 0.25 0.95, 0 0.5" />
+    				</clipPath>
+    			</defs>
+    		</svg>
+
+    		<svg class="clip-svg">
+    			<defs>
+    				<clipPath id="triangle-clip" clipPathUnits="objectBoundingBox">
+    					<polygon points="1 0.03, 0.17 1, 1 1" />
+    				</clipPath>
+    			</defs>
+    		</svg>
+    	</div>
     </v-layout>
   </v-container>
 </template>
@@ -62,8 +69,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+h1{
+  font-size: 20px;
+  margin-left: 25%;
 }
 ul {
   list-style-type: none;
@@ -76,4 +84,169 @@ li {
 a {
   color: #42b983;
 }
+
+body {
+  font-family: 'Roboto Condensed', sans-serif;
+  line-height: 1.5;
+}
+
+p {
+  margin-bottom: 40%;
+  text-align: center;
+  color: black;
+  font-size: 20px;
+}
+
+.wrap {
+  color: #222;
+  font-size: 24px;
+}
+
+.clip-svg {
+  width: 0;
+  height: 0;
+}
+
+.clip-caption {
+  width: 100%;
+  position: absolute;
+  top: 50%;
+  -ms-transform: translateY(-50%);
+  -webkit-transform: translateY(-50%);
+  transform: translateY(-50%);
+  text-align: center;
+}
+
+.clip-each {
+  display: block;
+  position: relative;
+  -webkit-clip-path: polygon(25% 5%, 75% 5%, 100% 50%, 75% 95%, 25% 95%, 0% 50%);
+  clip-path: polygon(25% 5%, 75% 5%, 100% 50%, 75% 95%, 25% 95%, 0% 50%);
+  -webkit-clip-path: url("#hexagon-clip");
+  clip-path: url("#hexagon-clip");
+  -webkit-transition: all 0.2s ease-in;
+  transition: all 0.2s ease-in;
+  margin: 0 auto;
+}
+
+a.clip-each:hover {
+  background: #ef6c00;
+  -webkit-transition: all 0.25s ease-in;
+  transition: all 0.25s ease-in;
+}
+
+.clip-solid {
+  width: 500px;
+  height: 500px;
+  background-color: #F5EDF0;
+  -webkit-transition: all 0.3s linear;
+}
+
+.clip-solid:after {
+  content: "";
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  -webkit-clip-path: polygon(100% 3%, 17% 100%, 100% 100%);
+  clip-path: polygon(100% 3%, 17% 100%, 100% 100%);
+  -webkit-clip-path: url("#triangle-clip");
+  clip-path: url("#triangle-clip");
+  background: #0071bc;
+  z-index: 1;
+  width: 100%;
+  height: 155px;
+  -webkit-transition: all 0.2s linear;
+  transition: all 0.2s linear;
+  pointer-events: none;
+}
+
+.clip-solid:hover:after {
+  height: 0;
+  -webkit-transition: all 0.45s ease-in;
+  transition: all 0.45s ease-in;
+}
+
+.clip-solid:hover .social-share-block {
+  -webkit-transition: all 0.2s linear;
+  transition: all 0.2s linear;
+}
+
+.social-share-block {
+  position: absolute;
+  bottom: 20px;
+  right: 75px;
+  color: #fff;
+  font-size: 70%;
+  z-index: 2;
+}
+
+.clip-gradient {
+  background: #64dd17;
+  width: 230px;
+  height: 230px;
+}
+
+.clip-gradient .clip-caption {
+  color: #F5EDF0;
+}
+
+.clip-border {
+  background-color: #e515e1;
+  width: 230px;
+  height: 230px;
+  margin-top: 7px;
+  color: white;
+}
+
+.clip-border::after {
+  content: "";
+  position: absolute;
+  top: 4%;
+  left: 4%;
+  right: 4%;
+  bottom: 4%;
+  margin: 0 auto;
+  -webkit-transition: all 0.3s linear;
+  transition: all 0.3s linear;
+  -webkit-clip-path: polygon(25% 5%, 75% 5%, 100% 50%, 75% 95%, 25% 95%, 0% 50%);
+  clip-path: polygon(25% 5%, 75% 5%, 100% 50%, 75% 95%, 25% 95%, 0% 50%);
+  -webkit-clip-path: url("#hexagon-clip");
+  clip-path: url("#hexagon-clip");
+  z-index: -1;
+}
+
+.clip-tagline {
+  display: block;
+  color: #758C9B;
+  text-decoration: none;
+  text-align: center;
+}
+
+@media only screen and (min-width: 550px) {
+  .wrap {
+    display: table;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -ms-transform: translateY(-50%) translateX(-50%);
+    -webkit-transform: translateY(-50%) translateX(-50%);
+    transform: translateY(-50%) translateX(-50%);
+  }
+
+  .clip-block {
+    display: table-cell;
+    vertical-align: middle;
+  }
+  img{
+    position:fixed;
+    max-height: 200px;
+
+    left: 0;
+  }
+  .clip-border {
+    margin-top: 20px;
+  }
+}
+
 </style>
