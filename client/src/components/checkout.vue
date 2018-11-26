90ebe9b2b0ec48a43ddd92a7fca7df2b1a214398
Erin de Blaeij <erinkayb@gmail.com>
2018-11-26 11:41:18 +0100

    fixes to ui of checkout page

--- client/src/components/checkout.vue
+++ client/src/components/checkout.vue
@@ -1,21 +1,27 @@
 <template>
   <v-container grid-list-xl text-md-center>
     <v-layout row wrap>
-      <v-flex xs6>
+      <v-flex xs6 offset-xs3>
         <div class="white elevation-24">
           <v-toolbar flat dense class='light-green accent-4' dark>
               <v-toolbar-title>
                 <v-flex>
-                  Checkout
+                  Donate
                 </v-flex>
               </v-toolbar-title>
             </v-toolbar>
             <div class='pl-4 pr-4 pt-4 pb-4'>
               <v-flex>
+                <p>Help us continue to make awesome games.</p>
                 <div class="input-group">
-                  <span class="input-group-addon">$</span>
-                  <input type="number" class="form-control" v-model="amount" aria-label="Amount (to the nearest dollar)">
-                  <span class="input-group-addon">.00</span>
+                  <v-text-field
+                    type="number"
+                    class="form-control"
+                    v-model="amount"
+                    prefix="$"
+                    suffix=".00"
+                    aria-label="Amount (to the nearest dollar)">
+                  </v-text-field>
                 </div>
                 <br><br><br>
                <paypal :amount="amount"></paypal>
